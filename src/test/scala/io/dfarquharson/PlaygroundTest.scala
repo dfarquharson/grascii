be5c38103addfbcf316fd1e69d26a42f8e6d11a4
David Farquharson <david.s.j.farquharson@gmail.com>
2020-04-19 22:45:33 -0500

    a little more list concat

--- src/test/scala/io/dfarquharson/PlaygroundTest.scala
+++ src/test/scala/io/dfarquharson/PlaygroundTest.scala
@@ -6,6 +6,8 @@ class PlaygroundTest extends FunSuite {
 
   test("List concat") {
     assert(List(1, 2, 3) ++ List(4, 5, 6) == List(1, 2, 3, 4, 5, 6))
+    assert(List(Person("A"), Person("B")) ++ List(Person("B"), Person("C")) ==
+      List(Person("A"), Person("B"), Person("B"), Person("C")))
   }
 
   test("Sets and Case Classes") {
